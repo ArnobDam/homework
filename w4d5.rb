9d44d0d68df7efefd9bafc06e7dba1c3fad76e4b
Arnob <arnobdam@gmail.com>
2022-10-08 16:29:30 -0400

    first half of w4d5 hw

---
+++ w4d5.rb
@@ -0,0 +1,74 @@
+def sluggish_octopus(arr)
+
+    longest_fish = ""
+    (0..arr.length-1).each do |i|
+        (0..arr.length-1).each do |j|
+            fish_1 = arr[i]
+            fish_2 = arr[j]
+            if fish_1.length > longest_fish.length && fish_1.length > fish_2.length
+                longest_fish = fish_1
+            elsif fish_1.length > longest_fish.length && fish_2.length > fish_1.length 
+                longest_fish = fish_2
+            end
+        end
+    end
+    longest_fish
+
+end
+#O(n^2)
+
+fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+# p sluggish_octopus(fish_arr)
+
+def dominant_octopus(arr)
+    merge_sort(arr).last
+end
+#O(nlog(n))
+
+def merge_sort(arr)
+    return arr if arr.length < 2
+
+    midpoint = arr.length / 2
+    left = arr[0...midpoint]
+    right = arr[midpoint..-1]
+    merge_sort(left)
+    merge_sort(right)
+
+    merge(left, right)
+end
+
+def merge(left, right)
+    merged_arr = []
+
+    while !left.empty? && !right.empty?
+        if (left.first <=> right.first) == -1
+            merged_arr << left.shift
+        else
+            merged_arr << right.shift
+        end
+    end
+
+       merged_arr.concat(left)
+       merged_arr.concat(right)
+       
+       merged_arr
+end
+
+# p dominant_octopus(fish_arr)
+
+def clever_octopus(arr)
+    longest_fish = arr.first
+
+    n = 0
+    while n < arr.length
+        longest_fish = arr[n] if arr[n].length > longest_fish.length
+        n += 1
+    end
+
+    longest_fish
+end
+
+p clever_octopus(fish_arr)
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
